5b06610963cac420f2cafa44848a755053de607b
pampakid <pamapkidx@gmail.com>
2024-11-01 19:37:12 -0700

    fix(general): Updates from the general repo review

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,10 @@
-# app/__init__.py
+# backend/app/__init__.py
 from flask import Flask
 from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+    CORS(app)  # This enables CORS for all routes
     
     # Import and register the blueprint
     from app.routes.todos import todo_bp
